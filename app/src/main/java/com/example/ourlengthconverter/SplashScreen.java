6d0d07c5c86aa2397d0d041ac09f226627c4bbc4
ArpaAlam01 <summaiya.tasnim1.cse@ulab.edu.bd>
2022-05-12 04:39:24 +0600

    Our Length Converter

---
+++ app/src/main/java/com/example/ourlengthconverter/SplashScreen.java
@@ -0,0 +1,47 @@
+package com.example.ourlengthconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private int progress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen2);
+
+        progressBar = findViewById(R.id.progressBarId);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doWork();
+            }
+        });
+        thread.start();
+    }
+    public void doWork()
+    {
+        for(progress=20; progress<=100; progress= +20)
+        {
+        try
+        {
+            Thread.sleep(1000);
+            progressBar.setProgress(progress);
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}}
